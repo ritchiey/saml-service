ac95abfff9b384962e3f9efdc7163249791b70a5
Bradley Beddoes <bradleybeddoes@gmail.com>
2014-09-02 13:57:52 +1000

    Enhance SamlUri factory for AttributeBase specs

--- spec/factories/saml_uris.rb
+++ spec/factories/saml_uris.rb
@@ -2,5 +2,12 @@ FactoryGirl.define do
   factory :saml_uri do
     uri 'urn:oasis:names:tc:SAML:2.0:protocol'
     type :protocol_support
+
+    to_create { |i| i.save }
+
+    factory :attribute_name_saml_uri do
+      uri 'urn:oasis:names:tc:SAML:2.0:attrname-format:uri'
+      type :attribute_name_format
+    end
   end
 end
