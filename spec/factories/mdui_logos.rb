174b403e118aae877176048a6c358d1e4a1da042
Bradley Beddoes <bradleybeddoes@gmail.com>
2014-11-26 12:49:21 +1000

    Ensure factory sets width/height greater than 0

--- spec/factories/mdui_logos.rb
+++ spec/factories/mdui_logos.rb
@@ -1,8 +1,8 @@
 FactoryGirl.define do
   factory :mdui_logo, class: 'MDUI::Logo' do
     uri { Faker::Internet.url }
-    width { Faker::Number.number(3) }
-    height { Faker::Number.number(3) }
+    width { Faker::Number.number(3).to_i + 1 }
+    height { Faker::Number.number(3).to_i + 2 }
     association :ui_info, factory: :mdui_ui_info
   end
 end
