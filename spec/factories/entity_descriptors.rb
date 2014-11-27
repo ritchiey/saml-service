2b5f2f9888c1f9f52c8a4922b5f097302da4292c
Bradley Beddoes <bradleybeddoes@gmail.com>
2014-11-27 14:24:33 +1000

    Extend factories to support MDRPI
    Allow both EntitiesDescriptors and EntityDescriptors to generate
    extended MDRPI related data.

--- spec/factories/entity_descriptors.rb
+++ spec/factories/entity_descriptors.rb
@@ -6,5 +6,16 @@ FactoryGirl.define do
       ed.entity_id = create :entity_id, entity_descriptor: ed
     end
 
+    trait :with_publication_info do
+      after(:create) do | ed |
+        ed.publication_info = create :mdrpi_publication_info, :with_usage_policy
+      end
+    end
+
+    trait :with_registration_info do
+      after(:create) do | ed |
+        ed.registration_info = create :mdrpi_registration_info, :with_policy
+      end
+    end
   end
 end
