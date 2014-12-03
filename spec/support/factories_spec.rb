0f3c77c8d6ef09322f1ad3b6c4da74c0550fc9bf
Bradley Beddoes <bradleybeddoes@gmail.com>
2014-12-03 11:04:59 +1000

    Ensure Factories spec runs within transaction
    Previously the generated specs for Factories and traits did not
    run within a transaction living the database full of garbage.

--- spec/support/factories_spec.rb
+++ spec/support/factories_spec.rb
@@ -3,12 +3,14 @@ require 'rails_helper'
 describe 'Factory Girl' do
   FactoryGirl.factories.map(&:name).each do |factory_name|
     describe "#{factory_name} factory" do
-
       it 'is valid' do
-        factory = FactoryGirl.build(factory_name)
-        if factory.respond_to?(:valid?)
-          expect(factory).to be_valid,
-                             -> { factory.errors.full_messages.join('\n') }
+        Sequel::Model.db.transaction(rollback: :always,
+                                     auto_savepoint: true) do
+          factory = FactoryGirl.build(factory_name)
+          if factory.respond_to?(:valid?)
+            expect(factory).to be_valid,
+                               -> { factory.errors.full_messages.join('\n') }
+          end
         end
       end
 
@@ -16,15 +18,17 @@ describe 'Factory Girl' do
       traits.map(&:name).each do |trait_name|
         context ":#{trait_name}" do
           it 'is valid' do
-            factory = FactoryGirl.build(factory_name, trait_name)
-            if factory.respond_to?(:valid?)
-              expect(factory).to be_valid,
-                                 -> { factory.errors.full_messages.join('\n') }
+            Sequel::Model.db.transaction(rollback: :always,
+                                         auto_savepoint: true) do
+              factory = FactoryGirl.build(factory_name, trait_name)
+              if factory.respond_to?(:valid?)
+                expect(factory).to be_valid,
+                                   -> { factory.errors.full_messages.join('\n') } # rubocop:disable Metrics/LineLength
+              end
             end
           end
         end
       end
-
     end
   end
 end
