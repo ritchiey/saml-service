a56d4dd34757ba6a4bd0be8e6b6a886136ea704e
Bradley Beddoes <bradleybeddoes@gmail.com>
2015-01-12 14:54:20 +1000

    DRY spec implementation

--- spec/metadata/saml_spec.rb
+++ spec/metadata/saml_spec.rb
@@ -19,95 +19,82 @@ RSpec.describe Metadata::SAML do
   end
 
   context 'Root EntitiesDescriptor' do
+    let(:entities_descriptor) { create :basic_federation }
+    let(:add_ca_keys) { true }
+
     let(:federation_identifier) { Faker::Internet.domain_word }
     let(:metadata_name) { "urn:mace:#{federation_identifier}.edu:test" }
     let(:metadata_validity_period) { 1.weeks }
+
+    let(:namespaces) { Nokogiri::XML.parse(raw_xml).collect_namespaces }
+
+    let(:entities_descriptor_path) { '/EntitiesDescriptor' }
+    let(:extensions_path) { '/EntitiesDescriptor/Extensions' }
+    let(:key_authority_path) { "#{extensions_path}/shibmd:KeyAuthority" }
+    let(:key_info_path) { "#{key_authority_path}/ds:KeyInfo" }
 
     before :each do
       subject.federation_identifier = federation_identifier
       subject.metadata_name = metadata_name
       subject.metadata_validity_period = metadata_validity_period
+      create_list(:ca_key_info, 2,
+                  entities_descriptor: entities_descriptor) if add_ca_keys
+
+      subject.root_entities_descriptor(entities_descriptor)
     end
 
-    context 'minimal instance' do
-      let(:entities_descriptor) { create :basic_federation }
-      let(:namespaces) { Nokogiri::XML.parse(raw_xml).collect_namespaces }
+    it 'is created' do
+      expect(xml).to have_xpath(entities_descriptor_path)
+    end
 
-      context 'EntitiesDescriptor' do
-        before { subject.root_entities_descriptor(entities_descriptor) }
-        it 'is created' do
-          path = '/EntitiesDescriptor[@ID and @Name and @validUntil]'
-          expect(xml).to have_xpath(path)
-        end
+    it 'defines namespaces' do
+      expect(namespaces).to eq(Metadata::SAML::NAMESPACES)
+    end
 
-        it 'defines namespaces' do
-          expect(namespaces).to eq(Metadata::SAML::NAMESPACES)
-        end
+    context 'attributes' do
+      let(:node) { xml.find(:xpath, entities_descriptor_path) }
 
-        context 'attributes' do
-          let(:node) { xml.find(:xpath, '/EntitiesDescriptor') }
+      around { |example| Timecop.freeze { example.run } }
 
-          around { |example| Timecop.freeze { example.run } }
+      it 'sets ID' do
+        expect(node['ID']).to start_with(federation_identifier)
+      end
+      it 'sets Name' do
+        expect(node['Name']).to eq(metadata_name)
+      end
+      it 'sets validUntil' do
+        expect(node['validUntil'])
+          .to eq((Time.now.utc + metadata_validity_period).xmlschema)
+      end
+    end
 
-          it 'sets ID' do
-            expect(node['ID']).to start_with(federation_identifier)
-          end
-          it 'sets Name' do
-            expect(node['Name']).to eq(metadata_name)
-          end
-          it 'sets validUntil' do
-            expect(node['validUntil'])
-              .to eq((Time.now.utc + metadata_validity_period).xmlschema)
-          end
+    context 'CA keys' do
+      context 'without CA keys' do
+        let(:add_ca_keys) { false }
+        it 'does not populate Extensions node' do
+          expect(xml).not_to have_xpath(extensions_path)
         end
       end
 
-      context 'CA keys' do
-        let(:extensions_path) { '/EntitiesDescriptor/Extensions' }
-        let(:key_authority_path) { "#{extensions_path}/shibmd:KeyAuthority" }
-        let(:key_info_path) { "#{key_authority_path}/ds:KeyInfo" }
-
-        context 'without CA keys' do
-          before { subject.root_entities_descriptor(entities_descriptor) }
-
-          it 'does not populate Extensions node' do
-            expect(xml).not_to have_xpath(extensions_path)
+      context 'with CA keys' do
+        context 'Extensions' do
+          it 'is created' do
+            expect(xml).to have_xpath(extensions_path)
           end
-        end
-
-        context 'with CA keys' do
-          let(:ca_key_info) do
-            create :ca_key_info, entities_descriptor: entities_descriptor
-          end
-          let(:ca_key_info2) do
-            create :ca_key_info, key_name: nil,
-                                 entities_descriptor: entities_descriptor
-          end
-          before do
-            entities_descriptor.add_ca_key_info(ca_key_info)
-            entities_descriptor.add_ca_key_info(ca_key_info2)
-            subject.root_entities_descriptor(entities_descriptor)
-          end
-
-          context 'Extensions' do
+          context 'KeyAuthority' do
             it 'is created' do
-              expect(xml).to have_xpath(extensions_path)
+              expect(xml).to have_xpath(key_authority_path)
             end
-            context 'KeyAuthority' do
-              it 'is created' do
-                expect(xml).to have_xpath(key_authority_path)
+            context 'attributes' do
+              let(:node) { xml.find(:xpath, key_authority_path) }
+              it 'sets VerifyDepth' do
+                expect(node['VerifyDepth'])
+                  .to eq(entities_descriptor.ca_verify_depth.to_s)
               end
-              context 'attributes' do
-                let(:node) { xml.find(:xpath, key_authority_path) }
-                it 'sets VerifyDepth' do
-                  expect(node['VerifyDepth'])
-                    .to eq(entities_descriptor.ca_verify_depth.to_s)
-                end
-              end
-              context 'KeyInfo' do
-                it 'creates two instances' do
-                  expect(xml).to have_xpath(key_info_path, count: 2)
-                end
+            end
+            context 'KeyInfo' do
+              it 'creates two instances' do
+                expect(xml).to have_xpath(key_info_path, count: 2)
               end
             end
           end
