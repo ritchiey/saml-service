1dac3ee4f6c0cb8bb0ac8ed22bcd0b610fb1c718
Bradley Beddoes <bradleybeddoes@gmail.com>
2015-07-08 16:27:38 +1000

    Add specs to ensure valid MDQP routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,19 @@
 Rails.application.routes.draw do
-  uri_regexp = URI.regexp(%w(http https urn:mace))
-  sha1_regex = /{sha1}(.*)?/
+  URI_REGEXP = URI.regexp(%w(http https urn:mace))
+  SHA1_REGEXP = /{sha1}(.*)?/
 
   match '/:primary_tag/entities',
         to: 'metadata_query#all_entities', via: :all
 
   match '/:primary_tag/entities/:identifier',
         to: 'metadata_query#specific_entity',
-        constraints: { identifier: uri_regexp }, via: :all
+        constraints: { identifier: URI_REGEXP }, via: :all
 
   match '/:primary_tag/entities/:identifier',
         to: 'metadata_query#specific_entity_sha1',
-        constraints: { identifier: sha1_regex }, via: :all
+        constraints: # check regexp against decoded URI params
+          -> (r) { r.path_parameters[:identifier].match(SHA1_REGEXP) },
+        via: :all
 
   match '/:primary_tag/entities/:identifier',
         to: 'metadata_query#tagged_entities', via: :all
