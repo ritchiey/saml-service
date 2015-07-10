893e4a2e6b146d3dd71343d4f6876ab164d039b4
Bradley Beddoes <bradleybeddoes@gmail.com>
2015-07-10 10:17:22 +1000

    DRY action implementations for MDQP
    ![less code matters]
    (http://36.media.tumblr.com/e2d90c8ae1be7d07e596e9ca3e27184a/tumblr_n3zmu6Dpv21ri9izgo1_540.png)

--- app/controllers/metadata_query_controller.rb
+++ app/controllers/metadata_query_controller.rb
@@ -12,23 +12,35 @@ class MetadataQueryController < ApplicationController
 
   def all_entities
     public_action
+
     return not_found unless @metadata_instance.all_entities
-
-    known_entities = KnownEntity.with_all_tags(@metadata_instance.primary_tag)
-    return not_found unless known_entities.present?
-
-    etag = generate_known_entities_etag(known_entities)
-    if known_entities_unmodified?(known_entities, etag)
-      return head :not_modified
-    end
-
-    create_known_entities_response(known_entities, etag)
+    tags = [@metadata_instance.primary_tag]
+    handle_entities_request(tags)
   end
 
   def tagged_entities
     public_action
 
     tags = [@metadata_instance.primary_tag, params[:identifier]]
+    handle_entities_request(tags)
+  end
+
+  def specific_entity
+    public_action
+
+    handle_entity_request(EntityId.first(uri: params[:identifier]))
+  end
+
+  def specific_entity_sha1
+    public_action
+
+    sha1_identifier = params[:identifier].match(SHA1_REGEX)
+    handle_entity_request(EntityId.first(sha1: sha1_identifier[1]))
+  end
+
+  private
+
+  def handle_entities_request(tags)
     known_entities = KnownEntity.with_all_tags(tags)
     return not_found unless known_entities.present?
 
@@ -40,36 +52,15 @@ class MetadataQueryController < ApplicationController
     create_known_entities_response(known_entities, etag)
   end
 
-  def specific_entity
-    public_action
+  def handle_entity_request(entity_id)
+    return not_found unless entity_id
 
-    known_entity = EntityId.first(uri: params[:identifier])
-                   .try(:parent).try(:known_entity)
-    return not_found unless known_entity
-
+    known_entity = entity_id.parent.known_entity
     etag = generate_descriptor_etag(known_entity)
     return head :not_modified if known_entity_unmodified?(known_entity, etag)
 
     create_known_entity_response(known_entity, etag)
   end
-
-  def specific_entity_sha1
-    public_action
-
-    sha1_identifier = params[:identifier].match(SHA1_REGEX)
-    return not_found unless sha1_identifier
-
-    known_entity = EntityId.first(sha1: sha1_identifier[1])
-                   .try(:parent).try(:known_entity)
-    return not_found unless known_entity
-
-    etag = generate_descriptor_etag(known_entity)
-    return head :not_modified if known_entity_unmodified?(known_entity, etag)
-
-    create_known_entity_response(known_entity, etag)
-  end
-
-  private
 
   def ensure_get_request
     return if request.get?
