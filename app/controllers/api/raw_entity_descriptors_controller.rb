b619918799cf0cc0668cdc49cefaeaef7b0803b6
Russell Ianniello <russell.ianniello@aaf.edu.au>
2016-05-31 07:34:04 +1000

    Persist RawEntityDescriptor and associated models
    Validates parameters and persists:
    - A new RawEntityDescriptor
    - A new KnownEntity
    - A new EntityId
    
    The "enabled" field is used from the payload when creating these models.
    
    Updated/created timestamps in the request are no longer used. Therefore
    the created/updated timestamps will be as per default. This means we do
    not store when an IdP was created (happens in Hosted IdP Service). From
    some exploration this shouldn't be an issue.
    
    Right now when a RawEntityDescriptor is created the response code is
    201. At this point a location header cannot be included (i.e. GET
    /api/entity_sources/:tag/raw_entity_descriptors/:id as this resource
    doesn't exist yet — something that may be required in the future.

--- app/controllers/api/raw_entity_descriptors_controller.rb
+++ app/controllers/api/raw_entity_descriptors_controller.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 module API
   class RawEntityDescriptorsController < APIController
+    VALID_URI_REGEX = /\A#{URI.regexp(%w(http https))}\z/
+    URL_SAFE_BASE_64_ALPHABET = /^[a-zA-Z0-9_-]+$/
+
     before_action do
       @entity_source = EntitySource[source_tag: params[:tag]]
       raise(ResourceNotFound) if @entity_source.nil?
@@ -9,19 +12,52 @@ module API
     def create
       check_access!(access_path)
       raise(BadRequest) unless valid_post_params?
-      render status: :ok, nothing: true
+      persist
+      render status: :created, nothing: true
     end
 
     private
 
+    def persist
+      Sequel::Model.db.transaction(isolation: :repeatable) do
+        ke = KnownEntity.create(entity_source: @entity_source,
+                                enabled: post_params[:enabled])
+        tag_known_entity(ke)
+        red = RawEntityDescriptor
+              .create(known_entity: ke, xml: post_params[:xml],
+                      enabled: post_params[:enabled], idp: true, sp: false)
+        EntityId.create(uri: post_params[:entity_id],
+                        raw_entity_descriptor: red)
+      end
+    end
+
+    def tag_known_entity(known_entity)
+      post_params[:tags].each { |t| known_entity.tag_as(t) }
+    end
+
     def post_params
       params.require(:raw_entity_descriptor)
-            .permit(:xml, :entity_id, :created_at, :updated_at, :enabled)
+            .permit(:xml, :entity_id, :enabled, tags: [])
     end
 
     def valid_post_params?
-      required_keys = [:xml, :entity_id, :created_at, :updated_at, :enabled]
-      required_keys.all? { |k| post_params.key? k }
+      required_keys? && valid_enabled_flag? && valid_entity_id? && valid_tags?
+    end
+
+    def valid_tags?
+      post_params[:tags].all? { |t| t =~ URL_SAFE_BASE_64_ALPHABET }
+    end
+
+    def valid_entity_id?
+      post_params[:entity_id] =~ VALID_URI_REGEX
+    end
+
+    def valid_enabled_flag?
+      [true, false].include?(post_params[:enabled])
+    end
+
+    def required_keys?
+      [:xml, :entity_id, :enabled, :tags].all? { |k| post_params.key? k }
     end
 
     def access_path
