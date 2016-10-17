f3437d44aee471bda3f36edd6a9dc77ffd65b62e
Bradley Beddoes <bradleybeddoes@gmail.com>
2016-10-17 16:28:08 +1000

    Address code readability concerns raised in PR

--- app/jobs/concerns/etl/service_providers.rb
+++ app/jobs/concerns/etl/service_providers.rb
@@ -86,7 +86,7 @@ module ETL
 
     def acs_attributes(acs, ac_data)
       ac_data[:attributes].each do |attr_data|
-        next if not_correctly_specified(attr_data)
+        next unless correctly_specified?(attr_data)
 
         base = fr_attributes[attr_data[:id]]
         ra = requested_attribute(acs, attr_data, base)
@@ -95,14 +95,13 @@ module ETL
       end
     end
 
-    def not_correctly_specified(attr_data)
+    def correctly_specified?(attr_data)
       # Don't represent attributes that require specification but
       # have not yet had specific values associated.
       # For example eduPersonEntitlement but no entitlement values
       # requested by the SP using our tooling as yet.
-      attr_data[:specificationRequired].present? &&
-        attr_data[:specificationRequired] &&
-        !attr_data[:values].present?
+      attr_data[:specificationRequired].blank? ||
+        (attr_data[:specificationRequired] && attr_data[:values].present?)
     end
 
     def requested_attribute(acs, attr_data, base)
