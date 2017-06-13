c7e9553efac1830898d89e592d0d251eee196ab6
Shaun Mangelsdorf <s.mangelsdorf@gmail.com>
2017-06-14 09:35:12 +1000

    Disable Gumboot ApplicationController examples

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -4,5 +4,8 @@ require 'rails_helper'
 require 'gumboot/shared_examples/application_controller'
 
 RSpec.describe ApplicationController, type: :controller do
-  include_examples 'Application controller'
+  # HACK: We can't use these examples until we can use AAF Lipstick. We're
+  # currently using Sequel instead of ActiveRecord, but Lipstick requires the
+  # latter, so that's not an option at this juncture.
+  # include_examples 'Application controller'
 end
