0c3c1ba17557fb536d93742c0ea501f519a97f2c
Bradley Beddoes <bradleybeddoes@gmail.com>
2019-03-20 11:13:54 +1000

    Fix failing DB migration for localized_names
    Previous version worked correctly for migration but failed with an
    unsupported error on rollback.
    
    Address with explicit `up`/`down` blocks.

--- db/migrate/20190225092700_alter_localized_names.rb
+++ db/migrate/20190225092700_alter_localized_names.rb
@@ -1,7 +1,13 @@
 Sequel.migration do
-  change do
+  up do
     alter_table :localized_names do
        set_column_type :value, 'varchar(2048)'
+    end
+  end
+
+  down do
+    alter_table :localized_names do
+       set_column_type :value, 'varchar(255)'
     end
   end
 end
