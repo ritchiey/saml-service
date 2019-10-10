f27620f8e3b6a1d38b2e3d6f54b511c605dca730
Vlad Mencl <vladimir.mencl@reannz.co.nz>
2019-10-11 12:01:05 +1300

    SAML: sort localized names and uris also by lang
    ... to prevent spurious metadata changes, in case the same name or URI (URL) gets registered multiple times with different languages.

--- lib/metadata/saml.rb
+++ lib/metadata/saml.rb
@@ -295,7 +295,7 @@ module Metadata
                                .registration_instant_utc.xmlschema
       }
       mdrpi.RegistrationInfo(ns, attributes) do |_|
-        rps = mi.registration_info.registration_policies { |ds| ds.order(:uri) }
+        rps = mi.registration_info.registration_policies { |ds| ds.order(:uri, :lang) }
         rps.each do |rp|
           mdrpi.RegistrationPolicy(rp.uri, 'xml:lang' => rp.lang)
         end
@@ -304,15 +304,15 @@ module Metadata
 
     def organization(org)
       root.Organization(ns) do |_|
-        org.organization_names { |ds| ds.order(:value) }.each do |name|
+        org.organization_names { |ds| ds.order(:value, :lang) }.each do |name|
           root.OrganizationName(name.value, 'xml:lang' => name.lang)
         end
 
-        org.organization_display_names { |ds| ds.order(:value) }.each do |dname|
+        org.organization_display_names { |ds| ds.order(:value, :lang) }.each do |dname|
           root.OrganizationDisplayName(dname.value, 'xml:lang' => dname.lang)
         end
 
-        org.organization_urls { |ds| ds.order(:uri) }.each do |url|
+        org.organization_urls { |ds| ds.order(:uri, :lang) }.each do |url|
           root.OrganizationURL(url.uri, 'xml:lang' => url.lang)
         end
       end
@@ -523,11 +523,11 @@ module Metadata
         isDefault: acs.default
       }
       root.AttributeConsumingService(ns, attributes) do |_acs_node|
-        acs.service_names { |ds| ds.order(:value) }.each do |service_name|
+        acs.service_names { |ds| ds.order(:value, :lang) }.each do |service_name|
           root.ServiceName(service_name.value, 'xml:lang' => service_name.lang)
         end
 
-        acs.service_descriptions { |ds| ds.order(:value) }.each do |desc|
+        acs.service_descriptions { |ds| ds.order(:value, :lang) }.each do |desc|
           root.ServiceDescription(desc.value, 'xml:lang' => desc.lang)
         end
 
@@ -581,29 +581,29 @@ module Metadata
 
     def ui_info(info)
       mdui.UIInfo(ns) do |_|
-        info.display_names { |ds| ds.order(:value) }.each do |display_name|
+        info.display_names { |ds| ds.order(:value, :lang) }.each do |display_name|
           mdui.DisplayName(display_name.value, 'xml:lang' => display_name.lang)
         end
 
-        info.descriptions { |ds| ds.order(:value) }.each do |description|
+        info.descriptions { |ds| ds.order(:value, :lang) }.each do |description|
           mdui.Description(description.value, 'xml:lang' => description.lang)
         end
 
-        info.keyword_lists { |ds| ds.order(:content) }.each do |keywords|
+        info.keyword_lists { |ds| ds.order(:content, :lang) }.each do |keywords|
           mdui.Keywords(keywords.content, 'xml:lang' => keywords.lang)
         end
 
-        info.logos { |ds| ds.order(:uri) }.each do |logo|
+        info.logos { |ds| ds.order(:uri, :lang) }.each do |logo|
           attributes = { height: logo.height, width: logo.width }
           attributes['xml:lang'] = logo.lang if logo.lang.present?
           mdui.Logo(logo.uri, attributes)
         end
 
-        info.information_urls { |ds| ds.order(:uri) }.each do |url|
+        info.information_urls { |ds| ds.order(:uri, :lang) }.each do |url|
           mdui.InformationURL(url.uri, 'xml:lang' => url.lang)
         end
 
-        info.privacy_statement_urls { |ds| ds.order(:uri) }.each do |url|
+        info.privacy_statement_urls { |ds| ds.order(:uri, :lang) }.each do |url|
           mdui.PrivacyStatementURL(url.uri, 'xml:lang' => url.lang)
         end
       end
