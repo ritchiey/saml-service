c0808186b23d32955f3a2ab5c0715d0a34fc4604
phyzical <5182053+phyzical@users.noreply.github.com>
2022-09-21 13:06:01 +0800

    move config higher

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,24 @@ module Saml
     # Settings in config/environments/* take precedence over those
     # specified here.
 
+    redis_user = ENV.fetch('REDIS_AUTH_TOKEN', nil).present? ? ":#{CGI.escape(ENV.fetch('REDIS_AUTH_TOKEN', nil))}@" : ''
+    config.saml_service =
+      RecursiveOpenStruct.new({
+        metadata: {
+          negative_cache_ttl: 600
+        },
+        api: {
+          authentication: :token
+        },
+        url_options: {
+          base_url: ENV.fetch('BASE_URL', nil)
+        },
+        redis: {
+          url: "#{ENV.fetch('REDIS_SCHEME', 'redis')}://#{redis_user}#{ENV.fetch('REDIS_HOST', 'localhost')}:6379/0"
+        },
+        version: "#{ENV.fetch('RELEASE_VERSION', 'OWO')}-#{ENV.fetch('SERIAL_NUMBER', 1)}"
+      }.deep_symbolize_keys)
+
     config.load_defaults 6.0
     config.autoloader = :zeitwerk
 
