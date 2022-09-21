d065ffa844eb72cd8c8045a2a62c1b2fa10d35f7
phyzical <5182053+phyzical@users.noreply.github.com>
2022-09-21 10:48:11 +0800

    adjust to only redis ping in production

--- app/controllers/health_controller.rb
+++ app/controllers/health_controller.rb
@@ -5,6 +5,6 @@ class HealthController < ApplicationController
   skip_after_action :ensure_access_checked
 
   def show
-    (Rails.application.config.cache_store == :null_store || Redis.new.ping) && Sequel::Model.db.test_connection
+    (Rails.env.production? && Redis.new.ping) && Sequel::Model.db.test_connection
   end
 end
